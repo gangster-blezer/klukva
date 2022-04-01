5c020aefbd82e75151a036beb0cd0091b46ee5fc
gangster-blezer <98465840+gangster-blezer@users.noreply.github.com>
2022-04-01 12:27:36 +0300

    reterterterter
    erterterterterterter

--- Soft/tkani.js
+++ Soft/tkani.js
@@ -5,6 +5,7 @@ function open_tkani() {
 	menu('black-part-on','black-part-off');
 	document.getElementsByClassName("vibor-tkani")[0].classList.add("vibor-tkani-opened");
 	document.getElementsByClassName("black-part-on")[0].addEventListener("click",close)
+	bodyfixed(true);
 }
 
 function menu(add,del) {
@@ -16,4 +17,21 @@ function close() {
 	document.getElementsByClassName("black-part-on")[0].removeEventListener("click",close)
 	menu('black-part-off','black-part-on');
 	document.getElementsByClassName("vibor-tkani")[0].classList.remove("vibor-tkani-opened");
+	bodyfixed(false);
 }
+
+function bodyfixed(a){
+	if(a){
+		document.body.classList.add("body-fixed");
+	}else{
+		document.body.classList.remove("body-fixed");
+	}
+}
+
+function changeSize() {
+	document.getElementsByClassName("on-size-btn")[0].classList.remove("on-size-btn");
+	this.classList.add("on-size-btn");
+}
+
+document.getElementsByClassName("size-btn")[0].addEventListener("click",changeSize)
+document.getElementsByClassName("size-btn")[1].addEventListener("click",changeSize)
